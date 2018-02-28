e85ce83ecd71fc2d2f74f74b923ce6a77e64d81e
user <mourad.elbouaananii@gmail.com>
2018-02-28 12:27:36 +0100

    Add Form module display Heros when added

--- SuperHero/src/app/home/home.component.ts
+++ SuperHero/src/app/home/home.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  BtnTxt: string = "Add Hero!";
+  itemCount: number;
+  newHero: string = "Green Lantern";
+  Heros = [];
+
   constructor() { }
-
   ngOnInit() {
+    this.itemCount = this.Heros.length;
   }
-
+  addHero() {
+    this.Heros.push(this.newHero);
+    this.newHero = '';
+    this.itemCount = this.Heros.length;
+  }
+  
 }
