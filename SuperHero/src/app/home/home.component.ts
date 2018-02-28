b871dbb8cdd60ed388962c76778751e151396a7a
user <mourad.elbouaananii@gmail.com>
2018-02-28 13:37:07 +0100

    Add some smooth animation when a hero is added

--- SuperHero/src/app/home/home.component.ts
+++ SuperHero/src/app/home/home.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger,style,transition,animate,keyframes,query,stagger } from "@angular/animations";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+
+    trigger('Heros', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+
+        query(':enter', stagger('300ms', [
+          animate('1s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .5}),
+            style({opacity: 1, transform: 'translateY(0)', offset: 1})
+          ]))
+        ]), {optional: true})
+      ])
+    ])
+
+  ]
 })
 export class HomeComponent implements OnInit {
 
   BtnTxt: string = "Add Hero!";
   itemCount: number;
   newHero: string = "Green Lantern";
-  Heros = [];
+  Heros = ["Deadpool","Wolverine","Moi"];
 
   constructor() { }
   ngOnInit() {
@@ -21,5 +39,5 @@ export class HomeComponent implements OnInit {
     this.newHero = '';
     this.itemCount = this.Heros.length;
   }
-  
+
 }
